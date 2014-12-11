01353c7e07047fba14f5ae6981fb45cb8517cf82
Matthew Yang <imatthewyang@gmail.com>
2014-12-11 12:55:33 -0800

    Initial Commit

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Matthew Yang on 12/11/14.
+//  Copyright (c) 2014 Tmrw, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Alcolator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Alcolator
+//
+//  Created by Matthew Yang on 12/11/14.
+//  Copyright (c) 2014 Tmrw, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
